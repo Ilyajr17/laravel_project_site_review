8e6c7786e2af588e1e3e314941107a4df9074e11
Ilya Zherebtsov <izherebcov@gmail.com>
2022-07-07 18:46:06 +0400

    Создание общего html шаблона, главной страницы, старницы о нас и кнопки с отзвывами

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('main_content')
+
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <main class="px-3">
+        <h1>Здесь будет информация о нашей компании!</h1>
+        <p class="lead">Очень подробное описание, об истории создания, об основателях, ценнотях и приоритетах.</p>
+    </main>
+</div>
+
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('main_content')
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+
+  <main class="px-3">
+    <h1>Это наша главная старница.</h1>
+    <p class="lead">Здесь будет размещаться информация о товарах или услугах в зависимости от того чем будем заниматься.</p>
+  </main>
+
+
+</div>
+@endsection
